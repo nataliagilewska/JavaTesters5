5f8e8cebca8db13634136902eaf28d5ecaa433ee
nataliagilewska <n.gilewska@tt.com.pl>
2019-05-29 22:57:41 +0200

    Zadanie z ksiazka adresowa

--- src/KsiazkaAdresowa.java
+++ src/KsiazkaAdresowa.java
@@ -1,10 +1,30 @@
 public class KsiazkaAdresowa {
     Kontakt[] kontakt; //= new Kontakt[200];
+    int rozmiar;
 
-    public KsiazkaAdresowa(Kontakt kontakt)
+    public KsiazkaAdresowa(int rozmiar)
     {
-        //this.kontakt=kontakt;
+        this.rozmiar=rozmiar;
+        kontakt = new Kontakt [rozmiar];
+         //this.kontakt=kontakt;
         //kontakt(kontakt.length)=kontakt;
+    }
+
+    void dodajKontakt(int pozycja, Kontakt kontakt2)
+    {
+        kontakt[pozycja]=kontakt2;
+    }
+
+    void pokazKontakty()
+    {
+        System.out.println("----------------------------");
+        System.out.println("Kontakty ksiazki adresowej: ");
+        for (int i=0; i<rozmiar; i++)
+        {
+            kontakt[i].wypisz();
+            System.out.println();
+        }
+        System.out.println("----------------------------");
     }
 
 }
