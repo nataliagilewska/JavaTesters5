66e44f7a3cdec6f62a0995489258fac7c87248b7
nataliagilewska <n.gilewska@tt.com.pl>
2019-05-29 22:58:38 +0200

    Zadanie z ksiazka adresowa

--- src/Kontakt.java
+++ src/Kontakt.java
@@ -4,6 +4,20 @@ public class Kontakt {
     int numerTelefonu;
     String email;
 
+    public Kontakt (String imie, String nazwisko, int numerTelefonu, String email)
+    {
+        this.imie=imie;
+        this.nazwisko=nazwisko;
+        this.numerTelefonu=numerTelefonu;
+        this.email=email;
 
+    }
 
+    public void wypisz()
+    {
+        System.out.println("Imie: "+ imie);
+        System.out.println("Nazwisko: "+ nazwisko);
+        System.out.println("Telefon: "+ numerTelefonu);
+        System.out.println("Email: "+email);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,22 @@ public class Main {
 
         //Autor natalia = new Osoba
         //Moge albo osobno dodac autora, albo od razu w ksiazce przypisac;
+        //Zad 1. w klasie Rodzaj
+        //Zad. 2
         Osoba jan = new Osoba ("Jan", "Kowalski");
         Ksiazka nowaKsiazka = new Ksiazka("Ksiazeczka", Rodzaj.HORRORY, 150, new Osoba("Natalia","Gilewska"));
         Ksiazka nowaKsiazka2= new Ksiazka ("Rozczarowania i dramaty", Rodzaj.DOKUMENTALNE,30, jan);
-        System.out.println("Pierwsza ksiazka: Tytul: "+nowaKsiazka.tytul+" Autor: "+nowaKsiazka.autor.imie+" "+nowaKsiazka.autor.nazwisko);
-        System.out.println("Druga ksiazka: Tytul: "+nowaKsiazka2.tytul+" Autor: "+nowaKsiazka2.autor.imie+" "+nowaKsiazka2.autor.nazwisko);
-
+        System.out.println("Druga ksiazka: Tytul: "+nowaKsiazka2.tytul+" Autor: ");
+        nowaKsiazka2.autor.przedstawSie();
+        //System.out.println("Pierwsza ksiazka: Tytul: "+nowaKsiazka.tytul+" Autor: "+nowaKsiazka.autor.imie+" "+nowaKsiazka.autor.nazwisko);
+        //System.out.println("Druga ksiazka: Tytul: "+nowaKsiazka2.tytul+" Autor: "+nowaKsiazka2.autor.imie+" "+nowaKsiazka2.autor.nazwisko);
+        int ilosckontaktow =4;
+        KsiazkaAdresowa ksiazkaAdresowa = new KsiazkaAdresowa(ilosckontaktow);
+        for (int i=0; i<ilosckontaktow; i++)
+        {
+            Kontakt k = new Kontakt("Jan"+i, "Kowalski", 123, "blabla@bla");
+            ksiazkaAdresowa.dodajKontakt(i, k);
+        }
+        ksiazkaAdresowa.pokazKontakty();
     }
 }
